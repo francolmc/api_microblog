125663a22efd2e5d8bb440a68df11c4a93daf1ec
Franco Morales Castro <franco.morales@outlook.com>
2025-10-02 11:17:21 -0300

    feat: filtro entre fechas

--- api_microblog/filters.py
+++ api_microblog/filters.py
@@ -3,7 +3,9 @@ from .models import Post
 
 class PostFilter(filters.FilterSet):
     content = filters.CharFilter(lookup_expr='icontains')
+    created_at_after = filters.DateFilter(field_name='created_at', lookup_expr='gte')
+    created_at_before = filters.DateFilter(field_name='created_at', lookup_expr='lte')
 
     class Meta:
         model = Post
-        fields = ['content']
+        fields = ['content', 'created_at_after', 'created_at_before']
