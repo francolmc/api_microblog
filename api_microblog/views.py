799d5e6bd4e47d397cb7cb0166f58b2790478e37
Franco Morales Castro <franco.morales@outlook.com>
2025-10-01 21:28:37 -0300

    feat: se implementala paginacion

--- api_microblog/views.py
+++ api_microblog/views.py
@@ -1,12 +1,14 @@
 from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
+from .pagination import CustomPagination
 
 # Create your views here.
 class PostListCreateView(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-
+    pagination_class = CustomPagination
+    
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
